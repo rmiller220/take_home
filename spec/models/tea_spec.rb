df90c91c656da190156d211597b62fbc2dfb6d29
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 15:52:22 -0600

    test relationships

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Tea, type: :model do 
   describe 'relationships' do
-    it { should belong_to(:subscription) }
+    it { should have_many(:subscription_teas) }
   end
 end
