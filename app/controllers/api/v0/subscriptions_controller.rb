88310cee5743f11a29b5ec365c9db4788edd4e7e
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 16:38:19 -0600

    add create method for new subscriptions

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,2 +1,18 @@
 class Api::V0::SubscriptionsController < ApplicationController
+
+  def create
+    # require 'pry'; binding.pry
+    subscription = Subscription.new(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: 201
+    else
+      render json: {error: "Something went wrong, no tea for you!"}, status: 400
+    end
+  end
+
+  private
+
+    def subscription_params
+      params.permit(:customer_id, :tea_id, :frequency, :title, :price, :status)
+    end
 end
