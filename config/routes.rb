15c8e63097a9861c80f3c867fe6f0b65703e2efb
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 16:38:31 -0600

    add routes using namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :teas
-  resources :subscriptions
-  resources :customers
+  
+  namespace :api do
+    namespace :v0 do
+      post '/subscribe', to: 'subscriptions#create'
+      delete '/unsubscribe', to: 'subscriptions#destroy'
+      get '/subscriptions', to: 'subscriptions#index'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
