f7b211e3cfcaab487d6a85d6828e9e0302490c54
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 16:37:28 -0600

    update migrations and models

--- db/migrate/20230725194210_create_subscriptions.rb
+++ db/migrate/20230725194210_create_subscriptions.rb
@@ -6,6 +6,7 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
       t.string :status
       t.string :frequency
       t.references :customer, null: false, foreign_key: true
+      t.references :tea, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_205324) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_194246) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,24 +20,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_205324) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "subscription_teas", force: :cascade do |t|
-    t.integer "subscription_id", null: false
-    t.integer "tea_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["subscription_id"], name: "index_subscription_teas_on_subscription_id"
-    t.index ["tea_id"], name: "index_subscription_teas_on_tea_id"
-  end
-
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.string "price"
     t.string "status"
     t.string "frequency"
     t.integer "customer_id", null: false
+    t.integer "tea_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -49,7 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_205324) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "subscription_teas", "subscriptions"
-  add_foreign_key "subscription_teas", "teas"
   add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
