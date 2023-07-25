38929c1b0b0cbdb9b26ffee9a94f63974b9a2307
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 16:37:50 -0600

    update model tests, add test data to spec

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,5 +3,15 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   describe 'relationships' do
     it { should belong_to(:customer) }
+    it { should belong_to(:tea) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:customer_id) }
+    it { should validate_presence_of(:tea_id) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
   end
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Tea, type: :model do 
   describe 'relationships' do
-    it { should have_many(:subscription_teas) }
+    it { should have_many(:subscriptions) }
   end
 end
