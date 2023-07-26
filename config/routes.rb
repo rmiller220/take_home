216de749fe2be99cd41269e777db64039c1507f7
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-26 14:29:50 -0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       post '/subscribe', to: 'subscriptions#create'
-      delete '/unsubscribe', to: 'subscriptions#destroy'
-      get '/subscriptions', to: 'subscriptions#index'
+      delete '/unsubscribe', to: 'customers#destroy'
+      get '/subscriptions', to: 'customers#show'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
